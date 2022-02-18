649a172000e9ff0132f91211b205b886270dd76c
Roger <roger@apis3.com>
2022-02-18 15:29:53 -0300

    seletores

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // #3 selecionando elemento
 // no console digite:
+// seleciona todo seletor de css3
 document.querySelector("#exampleInputName");
 
 // #4
@@ -32,9 +33,10 @@ console.log(el.value);
 
 /**
  * #7 Todos os elementos do formulário
+ * problemas encontrados na seleção dos elementos dessa forma
  */
 
-var name = document.querySelector("#exampleInputName");
+/*var name = document.querySelector("#exampleInputName");
 var gender = document.querySelector("#exampleInputGenderM");
 var gender = document.querySelector("#exampleInputGenderF");
 var birth = document.querySelector("#exampleInputBirth");
@@ -43,3 +45,33 @@ var email = document.querySelector("#exampleInputEmail1");
 var password = document.querySelector("#exampleInputPassword1");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#");
+*/
+/**
+ * #8 selecionar campos gender
+ * no console
+ * utilizar querySelectorAll p/ selecionar os 2 campos gender
+ * utilizar condição p/ selecionar a opção checada
+ */
+ document.querySelector('#form-user-create [name=gender]');
+ document.querySelectorAll('#form-user-create [name=gender]');
+ document.querySelectorAll('#form-user-create [name=gender]:checked');
+
+// Corrigindo a lista de campos selecionados
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+// recuperar campos/valores do formulário de forma acadêmica
+
+fields = document.querySelectorAll('#form-ser-create [name]');
+
+fields.forEach(function(field, index){
+
+    console.push(field, field.id, field.name, field.value, field.check, index)
+
+});

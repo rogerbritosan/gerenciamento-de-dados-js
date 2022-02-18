85f8ef7e9b643589eed1c7d245de3b6e666c81bf
Roger <roger@apis3.com>
2022-02-18 12:03:31 -0300

    campos formulário

--- index.js
+++ index.js
@@ -18,9 +18,28 @@ document.querySelector('#exampleInputName').value;
 
 /**
  * #6 variáveis de memória
+ * não pode começar com número 
+ * pode conter $ e _ pra começar
+ * não pode usar palavras reservadas da programação
+ * minúsculo e camelCase
  */
 document.querySelector("#exampleInputName");
 var el = document.querySelector("#exampleInputName");
 el.value;
+el.style.color = "blue";
 console.log(el);
 console.log(el.value);
+
+/**
+ * Todos os elementos do formulário
+ */
+
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#exampleInputGenderM");
+var gender = document.querySelector("#exampleInputGenderF");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail1");
+var password = document.querySelector("#exampleInputPassword1");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#");

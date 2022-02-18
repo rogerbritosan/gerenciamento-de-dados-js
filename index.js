3b43d054324701b63433867e9e4499766a491239
Roger <roger@apis3.com>
2022-02-18 11:52:35 -0300

    variáveis

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ document.querySelector("#exampleInputName");
 // atribuindo valor a um elemento
 // no console digite:
 document.querySelector("#exampleInputName").value = "Olá";
+
+/**
+ * #5 captar o valor do campo
+ * no console:
+ */
+document.querySelector('#exampleInputName').value;
+
+/**
+ * #6 variáveis de memória
+ */
+document.querySelector("#exampleInputName");
+var el = document.querySelector("#exampleInputName");
+el.value;
+console.log(el);
+console.log(el.value);

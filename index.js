a3370168e1a54150c80feacff16786bf8c0d709f
Roger <roger@apis3.com>
2022-02-21 12:00:46 -0300

    funções

--- index.js
+++ index.js
@@ -120,12 +120,36 @@ fields.forEach(function(field, index){
 //     });
 
 // });
+    
+var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
+
+function addLine(dataUser){
+
+    //console.log('addline', dataUser);
+
+    var tr = document.createElement('tr');
+
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+        `;
+
+    document.getElementById('table-users').appendChild(tr);
+
+}
 
 document.getElementById('form-user-create').addEventListener('submit', function(event){
     event.preventDefault();
-    
-    var fields = document.querySelectorAll('#form-user-create [name]');
-    var user = {};
 
     fields.forEach(function(field, index){
 
@@ -141,6 +165,6 @@ document.getElementById('form-user-create').addEventListener('submit', function(
 
     });
 
-    console.log(user);
+    addLine(user);
 
 });
